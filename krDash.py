64e4df3f91de61beae8b71b9a36bd35458484f6b
jawook <95268438+jawook@users.noreply.github.com>
2022-07-07 23:52:33 -0600

    giving up for tonight
    can't get the category limitation to work on.  FDOW isn't read as a column.  likely related to deepcopy??

--- krDash.py
+++ krDash.py
@@ -7,44 +7,121 @@ Created on Thu Jul  7 15:12:31 2022
 
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-cust = pd.read_csv('KiteRight Customers.csv', encoding_errors='ignore')
-cust['BirthDate'] = pd.to_datetime(cust['BirthDate'])
-cust['Income'] = cust['Income'].str.replace(r'[$,]', '', regex=True)
-cust['FullName'] = (cust['FirstName'].str.title() + ' ' + 
-                    cust['LastName'].str.title())
 
-sales = pd.read_csv('KiteRight Sales.csv')
-sales['InventoryDate'] = pd.to_datetime(sales['InventoryDate'])
-sales['SoldDate'] = pd.to_datetime(sales['SoldDate'])
-sales['SoldISOWeek'] = sales['SoldDate'].dt.isocalendar().week
-sales['SoldISOYear'] = sales['SoldDate'].dt.isocalendar().year
-sales['SoldISODoW'] = sales['SoldDate'].dt.dayofweek
-sales['SoldYear'] = sales['SoldDate'].dt.year
+@st.cache
+def loadMerge():
+    cust = pd.read_csv('KiteRight Customers.csv', encoding_errors='ignore')
+    cust['BirthDate'] = pd.to_datetime(cust['BirthDate'])
+    cust['Income'] = cust['Income'].str.replace(r'[$,]', '', regex=True)
+    cust['FullName'] = (cust['FirstName'].str.title() + ' ' + 
+                        cust['LastName'].str.title())
+    
+    sales = pd.read_csv('KiteRight Sales.csv')
+    sales['InventoryDate'] = pd.to_datetime(sales['InventoryDate'])
+    sales['SoldDate'] = pd.to_datetime(sales['SoldDate'])
+    sales['SoldISOWeek'] = sales['SoldDate'].dt.isocalendar().week
+    sales['SoldISOYear'] = sales['SoldDate'].dt.isocalendar().year
+    sales['SoldISODoW'] = sales['SoldDate'].dt.dayofweek
+    sales['SoldFDoW'] = sales['SoldDate'] - pd.TimedeltaIndex(sales['SoldDate'].dt.dayofweek, unit='d')
+    sales['SoldYear'] = sales['SoldDate'].dt.year
+    
+    regs = pd.read_csv('KiteRight Regions.csv')
+    
+    cats = pd.read_csv('KiteRight Categories.csv')
+    
+    prods = pd.read_csv('KiteRight Products.csv')
+    prods = pd.merge(left=prods, right=cats, on='CategoryKey', how='left')
+    prods['Twintip'] = [1 if j.endswith('cm') else 0 for j in prods['Size']]
+    prods['Surfboard'] = [1 if j.endswith('"') else 0 for j in prods['Size']]
+    
+    merge = sales.copy()
+    merge = pd.merge(left=sales, right=cust, on='CustomerKey', how='left')
+    merge = pd.merge(left=merge, right=prods, on='ProductKey', how='left')
+    merge = pd.merge(left=merge, right=regs, on='RegionKey', how='left')
 
-regs = pd.read_csv('KiteRight Regions.csv')
+    return merge
 
-cats = pd.read_csv('KiteRight Categories.csv')
+merge = loadMerge()
 
-prods = pd.read_csv('KiteRight Products.csv')
-prods = pd.merge(left=prods, right=cats, on='CategoryKey', how='left')
-prods['Twintip'] = [1 if j.endswith('cm') else 0 for j in prods['Size']]
-prods['Surfboard'] = [1 if j.endswith('"') else 0 for j in prods['Size']]
-
-merge = sales.copy()
-merge = pd.merge(left=sales, right=cust, on='CustomerKey', how='left')
-merge = pd.merge(left=merge, right=prods, on='ProductKey', how='left')
-merge = pd.merge(left=merge, right=regs, on='RegionKey', how='left')
-
-mindate = st.sidebar.date_input('First date shown: ', min(merge['SoldDate']), 
+#%% Constraints for page
+mindate = st.sidebar.date_input('First date shown: ', 
+                                min(merge['SoldDate']), 
                                 max_value=max(merge['SoldDate']), 
                                 min_value=min(merge['SoldDate']))
 
-maxdate = st.sidebar.date_input('Last date shown: ', max(merge['SoldDate']), 
+maxdate = st.sidebar.date_input('Last date shown: ', 
+                                max(merge['SoldDate']), 
                                 max_value=max(merge['SoldDate']), 
                                 min_value=min(merge['SoldDate']))
 
-limitMerge = merge[(merge['SoldDate'].dt.date >= mindate) & 
+catSel = st.sidebar.multiselect('Selected category for category limited visuals', 
+                                merge['CategoryName'].unique())
+
+#%% Global date data limitation
+dtLimitMerge = merge[(merge['SoldDate'].dt.date >= mindate) & 
                    (merge['SoldDate'].dt.date <= maxdate)]
-kiteData = limitMerge[limitMerge['CategoryName'] =='Kites'].groupby(by='Size')
-st.bar_chart(kiteData['SoldQuantity'].sum())
+
+#%% Creation of date-limited figures
+kiteData = dtLimitMerge[dtLimitMerge['CategoryName'] =='Kites'].groupby(by='Size')
+kiteFig = px.bar(kiteData['SoldQuantity'].sum().sort_values(),
+                 orientation='h', title='Kite Quantity Sold by Size',
+                 color_discrete_sequence=px.colors.qualitative.Vivid)
+kiteFig.layout.update(showlegend=False)
+
+sBrdData = dtLimitMerge[(dtLimitMerge['CategoryName'] =='Boards') &
+                      (dtLimitMerge['Surfboard'] == 1)].groupby(by='Size')
+sBrdFig = px.bar(sBrdData['SoldQuantity'].sum().sort_values(),
+                 orientation='h', title='Surfboard Quantity Sold by Size',
+                 color_discrete_sequence=px.colors.qualitative.Vivid)
+sBrdFig.layout.update(showlegend=False)
+
+tTipData = dtLimitMerge[(dtLimitMerge['CategoryName'] =='Boards') &
+                      (dtLimitMerge['Twintip'] == 1)].groupby(by='Size')
+tTipFig = px.bar(tTipData['SoldQuantity'].sum().sort_values(),
+                 orientation='h', title='Twintip Quantity Sold by Size',
+                 color_discrete_sequence=px.colors.qualitative.Vivid)
+tTipFig.layout.update(showlegend=False)
+
+clthData = dtLimitMerge[(dtLimitMerge['CategoryName'] == 
+                       'Clothing')].groupby(by='Size')
+clthFig = px.bar(clthData['SoldQuantity'].sum().sort_values(),
+                 orientation='h', title='Clothing Quantity Sold by Size',
+                 color_discrete_sequence=px.colors.qualitative.Vivid)
+clthFig.layout.update(showlegend=False)
+
+trmpFig = px.treemap(dtLimitMerge, 
+                     path=[px.Constant("All"), 'CategoryName'], 
+                     values='SoldQuantity', 
+                     title='Quantity Sold by Product Category',
+                     color_discrete_sequence=px.colors.qualitative.Vivid)
+
+#%% Creation of date and category limited figures
+
+if catSel==[]:
+    dcLimitMerge = dtLimitMerge.copy()
+else:
+    dcLimitMerge = dtLimitMerge[dtLimitMerge['CategoryName'].isin(catSel)]
+
+wkSalesFig = px.bar(dcLimitMerge,
+                    title='Orders by Week',
+                    x='SoldFDoW', y='SoldQuantity',
+                    color_discrete_sequence=px.colors.qualitative.Vivid)
+
+#%% Dashboard construction
+st.header('KiteRight: Analysis of Product Orders')
+st.subheader('Sales Quantity by Product Category')
+
+st.plotly_chart(trmpFig, use_container_width=True)
+
+quadChartL, quadChartR = st.columns(2)
+with quadChartL:
+    st.plotly_chart(kiteFig, use_container_width=True)
+    st.plotly_chart(sBrdFig, use_container_width=True)
+    
+with quadChartR:
+    st.plotly_chart(tTipFig, use_container_width=True)
+    st.plotly_chart(clthFig, use_container_width=True)
+    
+st.write(catSel)
