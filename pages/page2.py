2d0410aee64c7fc0ec28268807af5af9368b3636
jawook <95268438+jawook@users.noreply.github.com>
2022-07-08 17:02:53 -0600

    Create page2.py

---
+++ pages/page2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  8 16:57:26 2022
+
+@author: wilkijam
+"""
+
+import streamlit as st
+
+st.markdown('# This is the Second Page!')
+st.sidebar.markdown('# This is the Second Page')
