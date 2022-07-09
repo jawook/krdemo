1cb35d2b799c256d254e2fcff51cd7aa9a4ca1d0
jawook <95268438+jawook@users.noreply.github.com>
2022-07-09 00:09:02 -0600

    Completed, still needs polish

--- krDash.py
+++ krDash.py
@@ -12,6 +12,7 @@ Created on Thu Jul  7 15:12:31 2022
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import urllib.request, json
 
 px.defaults.color_discrete_sequence = px.colors.qualitative.Vivid
 
@@ -40,15 +41,41 @@ def loadMerge():
     prods = pd.merge(left=prods, right=cats, on='CategoryKey', how='left')
     prods['Twintip'] = [1 if j.endswith('cm') else 0 for j in prods['Size']]
     prods['Surfboard'] = [1 if j.endswith('"') else 0 for j in prods['Size']]
-    
+        
     merge = sales.copy()
     merge = pd.merge(left=sales, right=cust, on='CustomerKey', how='left')
     merge = pd.merge(left=merge, right=prods, on='ProductKey', how='left')
     merge = pd.merge(left=merge, right=regs, on='RegionKey', how='left')
-
+    
     return merge
 
 merge = loadMerge()
+
+#%% Get get lat/lon coordinates for mapping later
+def getCoJson():
+    urlCo = "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries.json"
+    with urllib.request.urlopen(urlCo) as url:
+        jsonCo = pd.DataFrame(json.loads(url.read().decode()))
+    jsonCo = jsonCo[['name', 'latitude', 'longitude']]
+    jsonCo.rename(columns={'name': 'Country'},
+                  inplace=True)
+    jsonCo['latitude'] = jsonCo['latitude'].astype(float)
+    jsonCo['longitude'] = jsonCo['longitude'].astype(float)
+    return jsonCo
+    
+def getProvJson():
+    urlProv = "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/states.json"
+    with urllib.request.urlopen(urlProv) as url:
+        jsonProv = pd.DataFrame(json.loads(url.read().decode()))
+    jsonProv = jsonProv[['name', 'latitude', 'longitude']]
+    jsonProv.rename(columns={'name': 'Province'},
+                    inplace=True)
+    jsonProv['latitude'] = jsonProv['latitude'].astype(float)
+    jsonProv['longitude'] = jsonProv['longitude'].astype(float)
+    return jsonProv
+
+jsonProv = getProvJson()
+jsonCo = getCoJson()
 
 #%% Constraints for page
 mindate = st.sidebar.date_input('First date shown: ', 
@@ -73,26 +100,26 @@ dtLimitMerge = merge[(merge['SoldDate'].dt.date >= mindate) &
 
 kiteData = dtLimitMerge[dtLimitMerge['CategoryName'] =='Kites'].groupby(by='Size')
 kiteFig = px.bar(kiteData['SoldQuantity'].sum().sort_values(),
-                 orientation='h', title='Kite Quantity Sold by Size')
-kiteFig.layout.update(showlegend=False)
+                 orientation='v', title='Kite Quantity Sold by Size')
+kiteFig.layout.update(showlegend=False, yaxis_title='', yaxis_tickformat=",.0d")
 
 sBrdData = dtLimitMerge[(dtLimitMerge['CategoryName'] =='Boards') &
                       (dtLimitMerge['Surfboard'] == 1)].groupby(by='Size')
 sBrdFig = px.bar(sBrdData['SoldQuantity'].sum().sort_values(),
-                 orientation='h', title='Surfboard Quantity Sold by Size')
-sBrdFig.layout.update(showlegend=False)
+                 orientation='v', title='Surfboard Quantity Sold by Size')
+sBrdFig.layout.update(showlegend=False, yaxis_title='', yaxis_tickformat=",.0d")
 
 tTipData = dtLimitMerge[(dtLimitMerge['CategoryName'] =='Boards') &
                       (dtLimitMerge['Twintip'] == 1)].groupby(by='Size')
 tTipFig = px.bar(tTipData['SoldQuantity'].sum().sort_values(),
-                 orientation='h', title='Twintip Quantity Sold by Size')
-tTipFig.layout.update(showlegend=False)
+                 orientation='v', title='Twintip Quantity Sold by Size')
+tTipFig.layout.update(showlegend=False, yaxis_title='', yaxis_tickformat=",.0d")
 
 clthData = dtLimitMerge[(dtLimitMerge['CategoryName'] == 
                        'Clothing')].groupby(by='Size')
 clthFig = px.bar(clthData['SoldQuantity'].sum().sort_values(),
-                 orientation='h', title='Clothing Quantity Sold by Size')
-clthFig.layout.update(showlegend=False)
+                 orientation='v', title='Clothing Quantity Sold by Size')
+clthFig.layout.update(showlegend=False, yaxis_title='', yaxis_tickformat=",.0d")
 
 trmpFig = px.treemap(dtLimitMerge, 
                      path=[px.Constant("All"), 'CategoryName'], 
@@ -113,7 +140,7 @@ dcLimitMerge = limCat(catSel, dtLimitMerge)
 
 wkSalesData = dcLimitMerge.groupby(by=['SoldFDoW', 'CategoryName']).sum()['SoldQuantity'].unstack()
 wkSalesFig = px.bar(wkSalesData)
-wkSalesFig.layout.update(showlegend=False)
+wkSalesFig.layout.update(showlegend=False, yaxis_tickformat=",.0d")
 
 sumTblData = dcLimitMerge.groupby(by=['CategoryName', 'Model']
                                   ).agg({'SoldQuantity': 'sum',
@@ -124,6 +151,23 @@ sumTblData.rename(columns={'CategoryName': 'Category',
                            'SoldQuantity': 'Quantity Sold',
                            'Price': 'Avg. Price'},
                   inplace=True)
+
+@st.cache
+def mapDriver(option):
+    if option == 'Province':
+        mapData = dcLimitMerge.groupby(by='Province'
+                                    ).agg({'SoldQuantity': 'sum'}
+                                              ).reset_index()
+        mapData = pd.merge(left=mapData, right=jsonProv, 
+                        on='Province', how='left')
+    else:
+        mapData = dcLimitMerge.groupby(by='Country'
+                                       ).agg({'SoldQuantity': 'sum'}
+                                             ).reset_index()
+        mapData = pd.merge(left=mapData, right=jsonCo, 
+                        on='Country', how='left')
+    mapData.dropna(inplace=True)
+    return mapData
 
 #%% Dashboard construction
 st.header('KiteRight: Analysis of Product Orders')
@@ -148,3 +192,11 @@ st.table(sumTblData.style.format({'Quantity Sold': "{:,}",
                                   'Avg. Price': "${:,.2f}"}))
 
 st.subheader('Orders by Region')
+radioMap = st.radio('Which geography level would you like to show?', 
+                    ['Country', 'Province'])
+mapData = mapDriver(radioMap)
+# mapFig = st.map(mapData)
+# mapFig
+mapFig2 = px.scatter_geo(mapData, lat='latitude', lon='longitude', 
+                         size='SoldQuantity', hover_name=radioMap)
+mapFig2
